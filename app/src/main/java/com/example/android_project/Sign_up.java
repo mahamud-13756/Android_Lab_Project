51030164949dce3d320e9d5b95506ceb04c10954
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-24 23:30:54 +0600

    Toast

--- app/src/main/java/com/example/android_project/Sign_up.java
+++ app/src/main/java/com/example/android_project/Sign_up.java
@@ -3,22 +3,44 @@ package com.example.android_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 public class Sign_up extends AppCompatActivity {
-    EditText name,email,age,contact,address;
+    EditText name_box,email_box,age_box,contact_box,address_box;
     Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        name=findViewById(R.id.Name);
-        email=findViewById(R.id.Email);
-        age=findViewById(R.id.Age);
-        contact=findViewById(R.id.Contact);
-        address=findViewById(R.id.Address);
+
+        name_box=findViewById(R.id.Name);
+        email_box=findViewById(R.id.Email);
+        age_box=findViewById(R.id.Age);
+        contact_box=findViewById(R.id.Contact);
+        address_box=findViewById(R.id.Address);
 
         signup=findViewById(R.id.btn_Sign_up);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = name_box.getText().toString();
+                Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();//kon page,kon data,koto time dekhabo
+
+
+                String add = address_box.getText().toString();
+                Toast.makeText(getApplicationContext(),add,Toast.LENGTH_LONG).show();
+
+               // String age = age_box.getText().toString();
+               // Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
